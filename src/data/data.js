ea9828a1d95d37d2f2521e96daeb76a49e55d530
rolando-d3v <landon3388@gmail.com>
2025-07-17 00:21:20 -0500

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,19 @@
-import git from "../assets/icons/git.png";
+
 import redes from "../assets/icons/redes.jpg";
 import js from "../assets/icons/js.png";
 import rol from "../assets/icons/rol_dev.png";
+import go from "../assets/icons/golang.png";
+import Bun from "../assets/icons/bun.png";
 
 export const data = {
   server: [
     { id: 1, name: "React", mensaje: 1, img: rol },
     { id: 2, name: "React", mensaje: 14, img: redes },
-    { id: 3, name: "Git", mensaje: 0, img: git },
-    { id: 4, name: "React", mensaje: 1, img: js },
+    { id: 3, name: "Bun", mensaje: 0, img: Bun },
+    { id: 4, name: "React", mensaje: 12, img: js },
+    { id: 5, name: "Bun", mensaje: 0, img: Bun },
+    { id: 6, name: "Go", mensaje: 0, img: go },
+    { id: 7, name: "React", mensaje: 1, img: js },
  
     
   ],
--- src/pages/layoutDash/buttom_server/ButtonServer.jsx
+++ src/pages/layoutDash/buttom_server/ButtonServer.jsx
@@ -1,20 +1,7 @@
-import React from "react";
 import css from "./buttom.module.css";
 import iconDiscord from "../../../assets/icons/discord.png";
-import dev from "../../../assets/icons/rolando_d3v.png";
-import git from "../../../assets/icons/git.png";
 
-export default function ButtonServer({
-  home,
-  notificacion,
-  mensaje,
-  serv,
-}) {
-
-
-  console.log(serv);
-  console.log(serv?.mensaje);
-  
+export default function ButtonServer({ home, notificacion, mensaje, serv }) {
   return (
     <button
       // isHome={isHome}
@@ -22,22 +9,22 @@ export default function ButtonServer({
       // mentions={mentions}
       style={{
         "--mensaje": `"${serv?.mensaje}" `,
-        backgroundImage: `url(${serv?.img})` ,
-        
       }}
+      // backgroundImage: `url(${serv?.img})`,
       className={`
-        ${css.btn_content}
         ${home && css.home}
-        ${notificacion && css.notificacion}
-        ${serv?.mensaje > 0 && mensaje  && css.mensajes}
+        ${!home && css.bola}
+        ${css.btn_content}
+        ${serv?.mensaje > 0 && mensaje && css.mensajes}
         `}
-     
-
-      // className={selected ? "active" : ""}
     >
-      {home && <img className={css.img_home} src={iconDiscord} alt="Discord" />}
-
-      {/* <img className={css.img_server}   src={dev} alt="Discord" /> */}
+      {home ? (
+        <div className={css.img_server_home}>
+          <img className={css.img_home} src={iconDiscord} alt="Discord" />
+        </div>
+      ) : (
+        <img className={css.img_server} src={serv?.img} alt="Discord" />
+      )}
     </button>
   );
 }
