337adde117dbfebf90f25f32977c436ee685e620
rolando-d3v <landon3388@gmail.com>
2025-07-21 00:38:40 -0500

    update

--- src/pages/layoutDash/layout/LayoutDash.jsx
+++ src/pages/layoutDash/layout/LayoutDash.jsx
@@ -2,7 +2,6 @@ import css from './layout.module.css'
 import Server from '../servidores/Server'
 import InfoUser from '../info_user/InfoUser'
 import ListCanales from '../list_canales/ListCanales'
-import NameServer from '../name_server/NameServer'
 import HeaderCanal from '../head_canal/HeaderCanal'
 import ContentData from '../content_data/ContetData'
 import ListUser from '../list_user/Listuser'
@@ -22,7 +21,6 @@ export default function LayoutDash() {
       <Head/>
       <Server/>
       <InfoUser/>
-      <NameServer/>
       <ListCanales/>
       <HeaderCanal  activeSide={activeSide}  setActiveSide={setActiveSide}/>
       <ListUser activeSide={activeSide}  setActiveSide={setActiveSide}/>
--- src/pages/layoutDash/list_canales/ListCanales.jsx
+++ src/pages/layoutDash/list_canales/ListCanales.jsx
@@ -1,102 +1,196 @@
 import css from "./list.module.css";
 import * as IconFa6 from "react-icons/fa6";
 import * as IconMd from "react-icons/md";
+import banner from "../../../assets/icons/banner.png";
+
+import icon from "../../../assets/icons/icon_d.png";
 
 export default function ListCanales() {
   return (
     <div className={css.list_content}>
-      <div className={css.categoria}>
-        <span> Texto </span>
-        <IconFa6.FaPlus className={css.icon_add} />
+
+      <div className={css.name_content}>
+        <div className={css.div_name_server}>
+          <img className={css.icon} src={icon} alt="icon" />
+          <h5 className={css.title}>Python en Español</h5>
+        </div>
+        <IconFa6.FaAngleDown className={css.icon_down} />
       </div>
 
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.div_banner}>
+          <img className={css.img_banner} src={banner} alt="baner" />
+        </div>
+      <section className={css.lista_canales}>
+        <div className={css.categoria}>
+          <span> Texto </span>
+          <IconFa6.FaPlus className={css.icon_add} />
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
-        </div>
-      </div>
-      
-      <div className={css.canal_button}>
-        <div>
-          <IconFa6.FaHashtag   className={css.icon_hash}  />
-          <span>Rolando dev2 </span>
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
 
-        <div>
-          <IconMd.MdPersonAddAlt1  className={css.icon_add_user} />
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
         </div>
-      </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+
+        <div className={css.canal_button}>
+          <div>
+            <IconFa6.FaHashtag className={css.icon_hash} />
+            <span>Rolando dev2 </span>
+          </div>
+
+          <div>
+            <IconMd.MdPersonAddAlt1 className={css.icon_add_user} />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
